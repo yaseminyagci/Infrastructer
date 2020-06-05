4de54e16a66a532da34bf0931b2fdf0349670cc6
Yasemin <y.yagci@metacortex.com.tr>
2020-06-05 15:37:11 +0300

    Infrastructer .net
    Infrastructer .net

---
+++ Data/Context/EFHadithContext.cs
@@ -0,0 +1,40 @@
+﻿using Core.CustomException;
+using Domain.Entity;
+//using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace Data.Context
+{
+    public class EFHadithContext /*: DbContext*/
+    {
+        //public EFHadithContext()
+        //    : base("BookingContext")
+        //{
+        //    this.Configuration.LazyLoadingEnabled = true;
+        //}
+
+        //public override int SaveChanges()
+        //{
+        //    var result = 0;
+        //    try
+        //    {
+        //        result = base.SaveChanges();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // özelleştir
+        //        throw new DatabaseException("DataBaseExcetion", ex);
+        //    }
+        //    return result;
+        //}
+
+     
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+   
+        //    base.OnModelCreating(modelBuilder);
+        //}
+    }
+}
